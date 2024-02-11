ef98ccd107fc1b235010faff2b301234f363758b
chautranln <150863727+chautranln@users.noreply.github.com>
2024-02-11 17:17:47 -0500

    second push

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "f5ea9b07b8787aa57b85afa577b3a1d4"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "feeab1bcd26e45e196dce9bc20030807"
